7ee1bdbd9c65aa06794eb8e9c0cd23f8edee5e0f
Chris Smola <csmola@tangogroup.com>
2014-04-23 19:55:13 -0600

    added __str_repeat

---
+++ functions/__str_repeat.sh
@@ -0,0 +1,34 @@
+## /* @function
+#   @usage __str_repeat <string> <num>
+#
+#   @output true
+#
+#   @description
+#   In cases where you need a small number of characters repeated a certain number
+#   of times, this function will solve your problem. It will send <string>
+#   repeated <num> times to STDOUT.
+#   description@
+#
+#   @dependencies
+#   `egrep`
+#   dependencies@
+#
+#   @returns
+#   0 - successful execution
+#   1 - missing or excessive arguments
+#   2 - value passed for <num> is not a number
+#   returns@
+#
+#   @file functions/__str_repeat.sh
+## */
+
+function __str_repeat {
+    [ $# != 2 ] && return 1
+    local str=$1 mult=$2 out i
+    ! egrep -q '^[0-9]+$' <<< $mult && return 2
+
+    for i in $( seq $mult ); do out="${out}${str}"; done
+
+    echo $out
+}
+export -f __str_repeat
